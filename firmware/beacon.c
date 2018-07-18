d474096c3c3645e0504090a529a7d86b32b2f8ef
Alexander Fasching <fasching.a91@gmail.com>
2018-07-18 21:00:24 +0200

    Control LED over USB

--- firmware/beacon.c
+++ firmware/beacon.c
@@ -15,6 +15,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdio.h>
+#include <stdbool.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -24,14 +26,69 @@
 #include "usbdrv.h"
 
 
+#define CTRL_GET_REGISTER 0
+#define CTRL_SET_REGISTER 1
+
+#define REG_LED 0
+
+#define STATUS_OK 0
+#define STATUS_ERROR 1
+
+char strbuf[64];
+
 static inline void led_init(void) { DDRB |= (1<<PB5); }
 static inline void led_on(void) { PORTB |= (1<<PB5); }
 static inline void led_off(void) { PORTB &= ~(1<<PB5); }
 static inline void led_toggle(void) { PORTB ^= (1<<PB5); }
+static inline bool led_value(void) { return !!(PORTB & (1<<PB5)); }
+static inline void led_set(bool v) { v ? led_on() : led_off(); }
 
 
-usbMsgLen_t usbFunctionSetup(uchar setupData[8])
+typedef struct {
+    uint16_t value;
+    uint16_t status;
+} response_t;
+
+
+usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
+    usbRequest_t *rq = (void *) &data[0];
+    static response_t response;
+
+    if (rq->bRequest == CTRL_GET_REGISTER) {
+        uint16_t address = rq->wIndex.word;
+
+        response.value = 0;
+        response.status = STATUS_ERROR;
+
+        switch (address) {
+            case REG_LED:
+                response.value = led_value();
+                response.status = STATUS_OK;
+                break;
+        }
+
+        usbMsgPtr = (void *) &response;
+        return sizeof(response);
+
+    } else if (rq->bRequest == CTRL_SET_REGISTER) {
+        uint16_t address = rq->wIndex.word;
+        uint16_t value = rq->wValue.word;
+
+        response.value = 0;
+        response.status = STATUS_ERROR;
+
+        switch (address) {
+            case REG_LED:
+                led_set(value);
+                response.status = STATUS_OK;
+                break;
+        }
+
+        usbMsgPtr = (void *) &response;
+        return sizeof(response);
+    }
+
     return 0;
 }
 
